cc17f7c424a1509fd57a96a8f833b982a86e877d
JoseLuis-S <shjose@iesrafaelalberti.es>
2025-04-08 18:51:57 +0200

    Librerias para compilar

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,14 @@
 plugins {
     kotlin("jvm") version "2.1.0"
+    application
+    id("com.github.johnrengelman.shadow") version "8.1.1"
 }
 
-group = "org.example"
+application {
+    mainClass.set("org.albertidam.insurancemanager")
+}
+
+group = "org.albertidam.insurancemanager"
 version = "1.0-SNAPSHOT"
 
 repositories {
@@ -21,4 +27,16 @@ tasks.test {
 }
 kotlin {
     jvmToolchain(21)
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+tasks.named<com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar>("shadowJar") {
+    archiveBaseName.set("GestionSeguros")    // Nombre personalizado
+    archiveVersion.set("1.0")                // Versión
+    archiveClassifier.set("")                // Sin sufijo -all
+    mergeServiceFiles()
+    exclude("META-INF/*.SF", "META-INF/*.DSA", "META-INF/*.RSA") // Evita errores de firma
 }
